97e17d043cc679ae9d9f1cce2d8eac1b8ee4611f
Cedric <cedric@makeleaps.com>
2018-05-08 11:33:18 +0900

    add optional parameter to use hidden widget instead of select

--- money/contrib/django/forms/widgets.py
+++ money/contrib/django/forms/widgets.py
@@ -6,11 +6,16 @@ class CurrencySelectWidget(forms.MultiWidget):
     Custom widget for entering a value and choosing a currency
     """
 
-    def __init__(self, choices=None, attrs=None):
+    def __init__(self, choices=None, attrs=None, hide_currency_select=False):
         widgets = (
             forms.TextInput(attrs=attrs),
-            forms.Select(attrs=attrs, choices=choices),
         )
+
+        if hide_currency_select:
+            widgets += (forms.HiddenInput(attrs=attrs),)
+        else:
+            widgets += (forms.Select(attrs=attrs, choices=choices),)
+
         super(CurrencySelectWidget, self).__init__(widgets, attrs)
 
     def decompress(self, value):
